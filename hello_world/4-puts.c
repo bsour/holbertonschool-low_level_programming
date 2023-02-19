30b1ca682af5623285d3395b65b80418c66d0d52
Your Name <you@example.com>
2023-02-19 05:32:46 -0800

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * program for printing string characters using puts
+ *
+ * Always returns 0;
+ */
+
+int main(void)
+{
+  puts("\Programming is like building a multilingual puzzle");
+  return (0);
+}
