64260006d864c0a7fe7d43e6c2c10184655dfe15
Your Name <you@example.com>
2023-02-19 05:38:50 -0800

    error fixes

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * program for printing string characters using puts
+ * main - Entry point
  *
- * Always returns 0;
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  puts("\Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
