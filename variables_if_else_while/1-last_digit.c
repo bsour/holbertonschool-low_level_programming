366884103624bb2df20fd52ce7730c4bf571657b
Your Name <you@example.com>
2023-02-20 07:21:47 -0800

    error fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,12 +12,12 @@
 int main(void)
 {
 	int n;
+	char first_message[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/*code goes here */
-	char first_message[] = "Last digit of";
 
 	printf("%s %d is %d and is ", first_message, n, n % 10);
 	if (n % 10 > 5)
