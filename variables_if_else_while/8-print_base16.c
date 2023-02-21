1e1ee7e91c114de84aba5523ae86a9f276b160c0
Your Name <you@example.com>
2023-02-20 17:53:43 -0800

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main prints all numbers of base 16
+ *
+ * Return: 0 on success
+ *
+ */
+
+int main(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	x = 'a';
+
+	while (x <= 'f')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
