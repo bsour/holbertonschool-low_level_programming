8ae687f6a3e304873fc21967e226ce5992d881ec
Your Name <you@example.com>
2023-02-20 17:08:09 -0800

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 single digit numbers starting from 0
+ *
+ * Return: 0 on success
+ *
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
