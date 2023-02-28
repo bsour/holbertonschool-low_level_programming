6a6bcc724ac69156b4f0c18eca1e5bd547545fd7
Your Name <you@example.com>
2023-02-28 03:10:10 -0800

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while(str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
