8d12b473d037d46340c68ee81e7a008bcf74c223
Your Name <you@example.com>
2023-03-04 19:46:48 -0800

    added task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the memory area
+ * @c: constant byte
+ *
+ * Return: a pointer when the character is found
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (NULL);
+}
