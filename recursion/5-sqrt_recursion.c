3e56acd5eac9eb30852d0f92be7e039d840b3740
Your Name <you@example.com>
2023-03-06 16:57:48 -0800

    different approach

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,53 +1,43 @@
 #include "main.h"
 
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
 /**
- * _sqrt_recursion - Computes the natural square root of a number
- * @n: The number to compute the square root of
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
- * Return: The square root of `n`, or -1 if `n` does not have a natural square root.
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-int _sqrt_recursion(int n)
+int find_sqrt(int num, int root)
 {
-    if (n < 0)
-    {
-	    return (-1);
-    }
-    else if (n == 0 || n == 1)
-    {
-	    return (n);
-    }
-    else
-    {
-	    return (_sqrt_helper(n, 1, n));
-    }
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * _sqrt_helper - Helper function for computing the natural square root of a number
- * @n: The number to compute the square root of
- * @start: The starting value to search from
- * @end: The ending value to search to
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
  *
- * Return: The square root of `n`, or -1 if `n` does not have a natural square root.
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
-int _sqrt_helper(int n, int start, int end)
+int _sqrt_recursion(int n)
 {
-    int mid = (start + end) / 2;
+	int root = 0;
 
-    if (start > end)
-    {
-	    return (-1);
-    }
-    else if (mid * mid == n)
-    {
-	    return (mid);
-    }
-    else if (mid * mid > n)
-    {
-	    return (_sqrt_helper(n, start, mid - 1));
-    }
-    else
-    {
-	    return (_sqrt_helper(n, mid + 1, end));
-    }
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
