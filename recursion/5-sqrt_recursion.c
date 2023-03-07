87fea70af8bd97704f1d5e53787df5b5ef129577
Your Name <you@example.com>
2023-03-06 16:37:58 -0800

    added q 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sq_root - finds the natural square root of a number recursively
+ * @x: the number to find the square root of
+ * @y: the current candidate for the square root
+ *
+ * Return: the square root of x if it exists, or -1 if it doesn't
+ */
+
+int _sq_root(int x, int y)
+{
+	if (y * y == x)
+	{
+		return (y);
+	}
+	else if (y * y > x)
+	{
+		return (_sq_root(x, y + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sq_root(n, 1));
+}
