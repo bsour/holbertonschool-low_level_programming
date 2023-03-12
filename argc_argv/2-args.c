0f00579a00b05309c91287748e6da612e14d5751
Your Name <you@example.com>
2023-03-11 20:50:10 -0800

    2.1-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,13 +7,13 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argc[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
