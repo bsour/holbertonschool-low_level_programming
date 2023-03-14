49c1919ec49b1242e55336748e1b205da98a1eb3
Your Name <you@example.com>
2023-03-14 00:43:51 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *create_array - program startup
+ * @size: int being evaluated
+ * @c: char being evaluated
+ * Return: (0)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
