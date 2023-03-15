c6f28348eb3f00b901796307f47806a95f08b4d4
Your Name <you@example.com>
2023-03-15 02:02:21 -0700

    error fixes

---
+++ more_malloc_free/1-string_noncat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings, up to n bytes of s2
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the maximum number of bytes of s2 to concatenate
+ *
+ * Return: a pointer to the concatenated string, or NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int length = n;
+	unsigned int string;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (string = 0; s1[string]; string++)
+	{
+		length++;
+	}
+	concat = malloc(sizeof(char) * (length + 1));
+	if (concat == NULL)
+	{
+		return NULL;
+	}
+	length = 0;
+	for (string = 0; s1[string]; string++)
+	{
+		concat[length++] = s1[string];
+	}
+	for (string = 0; s2[string] && string < n; string++)
+	{
+		concat[length++] = s2[string];
+	}
+	concat[length] = '\0';
+	return (concat);
+}
