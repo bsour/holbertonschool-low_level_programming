8025a5a03c23dbf361fdf3e2d0edb1d2def8aee1
Your Name <you@example.com>
2023-03-22 05:14:57 -0700

    syntax changes to code to reduce character count , nil logic change

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,13 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	if (array && action)
-	{
-		i = 0;
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
-	}
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
