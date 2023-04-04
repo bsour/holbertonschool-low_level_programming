0c119add53666732580a1549f7e04e25278fd080
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-04 04:37:03 -0700

    fixed compilation errros

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -5,14 +5,18 @@
  *
  * Return: The number of elements in the list.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
     size_t count = 0;
 
+    if (h == NULL)
+    {
+	    return (0);
+    }
     while (h != NULL)
     {
-        h = h->next;
-        count++;
+	    count++;
+	    h = h->next;
     }
 
     return (count);
