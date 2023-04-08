39ce46255443b030266cc90ef84c1a6b37f630c2
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-07 18:57:54 -0700

    fixed formatting

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n) of a linked list
  * @head: pointer to the head of the list
  * Return: sum of all the data, or 0 if the list is empty
  */
