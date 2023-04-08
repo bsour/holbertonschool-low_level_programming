fe3f6f5745d124bc0ca7a4b806124c3f33f21612
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-07 18:32:29 -0700

    fixed kernel style

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,13 +3,15 @@
  * free_dlistint - frees a dlistint_t list
  * @head: pointer to the head of the list
  */
+
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    while (head != NULL) {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
