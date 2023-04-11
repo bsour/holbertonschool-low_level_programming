964baa62fa2bbb6b435fb7726651a31f21f60269
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-10 20:42:57 -0700

    error check

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,38 +10,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int ret;
-	int len;
+    if (!filename)
+        return (-1);
 
-	if (!filename)
-	{
-		return (-1);
-	}
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd < 0)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-		{
-			len++;
-		}
-	}
+    if (text_content)
+    {
+        int bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written < 0)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
-	{
-		return (-1);
-	}
-
-	if (text_content)
-	{
-		ret = write(fd, text_content, len);
-		if (ret < 0)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (1);
+    close(fd);
+    return (1);
 }
