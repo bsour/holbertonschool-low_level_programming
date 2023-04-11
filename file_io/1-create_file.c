514dd50b4bb5368932dad89efe4dc78b3d94c689
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-10 20:39:00 -0700

    added correct implimentation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,16 +10,38 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    FILE *fptr;
-    int i;
+	int fd;
+	int ret;
+	int len;
 
-    fptr = fopen(filename, "w");
-    if (fptr == NULL) {
-        return -1;
-    }
-    for (i = 0; text_content[i] != '\0'; i++) {
-        fputc(text_content[i], fptr);
-    }
-    fclose(fptr);
-    return 0;
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		ret = write(fd, text_content, len);
+		if (ret < 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
