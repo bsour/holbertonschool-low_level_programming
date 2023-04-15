11544407968b90681da348219770d5b955cece1f
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-15 07:40:11 -0700

    error fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **node_array;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table = NULL)
+	if (hash_table == NULL)
 		return (NULL);
 
 	node_array = malloc(sizeof(hash_node_t *) * size);
