b9ae014a5b8291f0598d67d96e29b4ae8713a740
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-22 17:57:02 -0700

    added header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
+
+/**
+ * Header file for including function prototypes
+ */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+
+
+#endif /* SEARCH_ALGOS_H */
