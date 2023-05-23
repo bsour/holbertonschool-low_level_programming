7df6fc1342665bb611f30851c552450a43bf8a18
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-22 18:10:20 -0700

    fixed header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,17 +1,14 @@
-#define SEARCH_ALGOS_H
 #ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * Header file for including function prototypes
+ * header file to include function prototypes
  */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
-
-
-
 
 #endif /* SEARCH_ALGOS_H */
