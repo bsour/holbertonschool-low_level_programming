530142b0ebaf77e4eb59af9b81e5a655ee7167dc
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-22 17:56:01 -0700

    added linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+/**
+ * Linear search - Searcheas for a value in an array
+ * of integers
+ * @array: Pointer to the first element in the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return: -1 or the index where the value is located
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+        size_t index;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	index = 0;
+	for (index; index < size; index++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", index,
+		       array[index]);
+		if (array[index] == value)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
